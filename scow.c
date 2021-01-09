44f6d44772826f4384c4b450a0d80cd7c086d8cb
cassepipe <cassepipe@ymail.com>
2021-01-09 17:25:24 +0100

    Before mkdir -p attempt

--- scow.c
+++ scow.c
@@ -27,12 +27,24 @@
 ** Scow is licensed under GPL3 .
 */
 
-in_in_set();
+char dotfiles_path[1000];
+
+typedef enum e_mode {
+	COLLECT,
+	DEPLOY,
+	INVADE,
+	TAKEOFF,
+} e_mode;
+
+int in_in_set();
 link_and_record_path_rec();
 deploy_links_from_paths_backup();
 deploy_links_from_paths();
 restore_from_backup();
-is_dir();
+int is_dir();
+e_mode parse_mode();
+
+void replicate_dir_structure();
 
 int link_and_record_path_rec(int argc, char *path)
 {
@@ -40,25 +52,21 @@ int link_and_record_path_rec(int argc, char *path)
 	if (path[path_len] == '/')
 		path[path_len] = '\0';
 
+
 	char target[256];
 	char *sep = strrchr(path, '/');
 	strcpy(target, sep + 1);
+
 	if (is_dir(target))
 	{
 		replicate_dir_structure(path);
 	}
 	else
 	{
-
-		char dest[1000];
-		strcpy(dest, getenv("HOME"));
-		strcat(dest, "/dotfiles/");
-
 		char new_link[1000];
-		strcpy(new_link, dest);
+		strcpy(new_link, dotfiles_path);
 		strcat(new_link, target);
 
-		mkdir(dest, 0700)
 
 		if (link(path, new_link) < 0)
 		{
@@ -83,13 +91,6 @@ void replicate_dir_structure(char *path, char *dest)
 }
 
 
-typedef enum e_mode {
-	ERROR,
-	COLLECT,
-	DEPLOY,
-	INNVADE,
-	TAKEOFF,
-} e_mode;
 
 e_mode parse_args(int argc, char *mode)
 {
@@ -101,36 +102,40 @@ e_mode parse_args(int argc, char *mode)
 
 	i = 0;
 	len = strlen(mode);
-	if (!strncmp(mode, deploy, len)
+	if (!strncmp(mode, deploy, len))
 			return DEPLOY;
-	if (!strncmp(mode, invade, len)
+	if (!strncmp(mode, invade, len))
 			return INVADE;
-	if (!strncmp(mode, deploy, len)
+	if (!strncmp(mode, takeoff, len))
 			return TAKEOFF;
 	return COLLECT;
 }
 
-int  main()
+int  main(int argc, char *argv[])
 {
 	e_mode mode;
 
+	//parsing args
 	mode = COLLECT;
 	if (argc < 2)
 	{
 			fprintf(stderr, "You must provide a file name\n");
-			return (ERROR);
+			return (1);
 	}
 	if (argc > 2)
 		mode = parse_mode(argc, argv[2]);
 
+	//get dotfiles directory path
+	strcpy(dotfiles_path, getenv("HOME"));
+	strcat(dotfiles_path, "/dotfiles/");
+	mkdir(dotfiles_path, 0700);
 
+	//dispatch
 	--argc;
 	switch (mode)
 	{
-		case ERROR:
-			return 1;
 		case COLLECT:
-			link_and_record_path_rec(argv[argc]);
+			link_and_record_path_rec(argc, argv[argc]);
 		case DEPLOY:
 			deploy_links_from_paths_backup(argv[argc]);
 		case INVADE:
